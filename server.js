83f41c6bf325850ae238f7fa6aa2e07874dff3ad
PRIYAM <you@example.com>
2025-03-09 22:56:06 +0530

    added cors to server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const errorHandler = require("./middleware/errorHandler");
 const cloudinary = require("cloudinary");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -22,6 +23,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Working");
