15ded6dd0daa7d9004283a710686154ce219f9bb
PRIYAM <you@example.com>
2025-03-19 17:21:34 +0530

    added helmet

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cloudinary = require("cloudinary");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
+const helmet = require("helmet");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +24,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(helmet());
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
